9650a2624829e10de81053d203f33e360e5f5b37
Martin78910 <martin4o78@gmail.com>
2016-08-12 13:33:05 +0300

    Data-Types-and-Methods LAB

---
+++ Programming Fundamentals/Exercises19May_Data-Types-and-Methods_LAB/Exercise19May/Exercise1/ExerciseOne.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1
+{
+    class ExerciseOne
+    {
+        static void Main(string[] args)
+        {
+            
+            byte centuries = byte.Parse(Console.ReadLine());
+            ushort years = (ushort)(centuries * 100);
+            uint days = (uint)(years * 365.2422);
+            uint hours = days * 24;
+            ulong minutes = 60 * hours;
+            Console.WriteLine("{0} centuries = {1} years = {2} days = {3} hours = {4} minutes", centuries,years,days,hours,minutes);
+        }
+    }
+}
---
+++ Programming Fundamentals/Exercises19May_Data-Types-and-Methods_LAB/Exercise19May/Exercise9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string animalType = Console.ReadLine();
+            switch (animalType)
+            {
+                case "dog": Console.WriteLine("mammal"); break;
+                case "snake": Console.WriteLine("reptile"); break;
+                case "crocodile": Console.WriteLine("reptile"); break;
+                case "tortoise": Console.WriteLine("reptile"); break;
+               
+                default: Console.WriteLine("unknown");
+                    break;
+            }
+        }
+    }
+}
